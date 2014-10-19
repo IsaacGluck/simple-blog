ca7cd7da0552c494fc4a2f08a92a976b9cd079fa
IsaacGluck <isaacbgluck@gmail.com>
2014-10-19 14:52:38 -0400

    auto create

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 ##### \/DATABASE\/ #####
 DATABASE = "blog.db"
 
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 def get_db():
     db = getattr(g, '_blog', None)
     if db is None:
---
+++ schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE posts (title TEXT, post TEXT);
+CREATE TABLE comments (title TEXT, comment TEXT);
