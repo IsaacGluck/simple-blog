9d410c70f994b5e46a0d06f72c7da26c69a15fc6
IsaacGluck <isaacbgluck@gmail.com>
2014-10-19 02:16:07 -0400

    yeah

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index.html")
+def index():
+	return render_template("index.html")
+
+@app.route("/create.html")
+def create():
+	return render_template("create.html")
+
+
+if __name__=="__main__":
+	app.run(host='0.0.0.0', port=8080, debug=True)
