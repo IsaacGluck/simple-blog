38b08d67442a5f587ddcd9aae030449bf8eb74f5
levakabas <lakabas15@gmail.com>
2014-10-21 13:09:51 -0400

    Table of usernames and passwords

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
-CREATE TABLE posts (title TEXT UNIQUE, post TEXT);
-CREATE TABLE comments (title TEXT UNIQUE, comment TEXT);
+CREATE TABLE posts (title TEXT UNIQUE, post TEXT, username TEXT);
+CREATE TABLE comments (title TEXT UNIQUE, comment TEXT, username TEXT);
+CREATE TABLE users (username TEXT UNIQUE, password TEXT);
